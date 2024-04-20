2b8a2cb77a4f798216ce1e0ad2b0c93a606d2c27
Evie <danieleverland@gmail.com>
2024-04-20 16:20:00 +0200

    Create fishing bob

---
+++ Fishton/Assets/Scripts/Fishing/FishingBob.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingBob : MonoBehaviour
+{
+    private EventManager Events;
+    private GameObject RendererObject;
+
+    public void Initialize(EventManager OwningEventManager)
+    {
+        Events = OwningEventManager;
+        Events.OnFishEncounterFinished += OnFinished;
+    }
+
+    private void Awake()
+    {
+        RendererObject = GetComponentInChildren<Renderer>().gameObject;
+    }
+
+    private void OnFinished()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        RendererObject.transform.localPosition = new Vector3(0.0f, Mathf.Sin(Time.time) / 6.0f, 0.0f);
+    }
+}
