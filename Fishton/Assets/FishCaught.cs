073732bce5476bc91382945d2d790998e959d0e7
KuMoJo <58419310+KuMoJo@users.noreply.github.com>
2024-04-21 11:58:35 +0200

    Made a fish fly towards the player that caught it when you win the minigame

--- Fishton/Assets/FishCaught.cs
+++ Fishton/Assets/FishCaught.cs
@@ -1,15 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class FishCaught : MonoBehaviour
+public class FishCaught : NetworkBehaviour
 {
     [SerializeField]
     private GameObject fish;
 
+    [SerializeField]
+    private bool activate = false;
 
-    public void CatchFish(GameObject fish)
+    private EventManager Events { get; set; }
+
+
+    private void Awake()
     {
-        GameObject fishInstance = Instantiate(fish, new Vector3(0, 20, 0), Quaternion.identity);
+        Events = EventManager.Get(gameObject);
+        Events.OnFishingStateChanged += OnFishingStateChanged;
     }
+
+    void Update()
+    {
+        if (activate == true)
+        {
+            CatchFish(fish);
+
+            activate = false;
+        }
+    }
+
+    private void OnFishingStateChanged(FishEncounterState newState)
+    {
+        if (newState == FishEncounterState.Succeeeded)
+        {
+            CatchFish(fish);
+        }
+    }
+
+    private void CatchFish(GameObject fish)
+    {
+        GameObject instancedFish = Instantiate(fish, new Vector3(0, 20, 0), Quaternion.identity);
+        instancedFish.GetComponent<AnimateCaughtFish>().CatchingPlayer = gameObject;
+    } 
 }
