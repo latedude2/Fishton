851682ba444cc11a55545893af9833c9a70b6c36
Evie <danieleverland@gmail.com>
2024-04-21 10:32:44 +0200

    Made minigame pretty(er)

--- Fishton/Assets/Scripts/Fishing/FishDefinition.cs
+++ Fishton/Assets/Scripts/Fishing/FishDefinition.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class FishDefinition : ScriptableObject
 {
-    
+    [Tooltip("The time the player has to catch the fish when it becomes hooked")]
+    public float TimeToCatch = 0.2f;
 }
