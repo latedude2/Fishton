19723bece152598e4c55e6e30032c434cfd39f80
JapierniczeK <36990086+JapierniczeK@users.noreply.github.com>
2024-06-16 14:54:48 +0200

    just some animation testing

--- Fishton/Assets/Scripts/Player/AnimationController.cs
+++ Fishton/Assets/Scripts/Player/AnimationController.cs
@@ -11,6 +11,7 @@ public class AnimationController : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         animator.SetInteger("FishingState", 0);
+        animator.speed = 0.5f;
         Events = EventManager.Get(gameObject);
         Events.OnFishingStateChanged += OnFishingStateChanged;
     }
@@ -20,12 +21,13 @@ public class AnimationController : MonoBehaviour
         PlayAnimation(GetAnimationStateNumberFromState(NewState));
         if(NewState == FishEncounterState.Finished || NewState == FishEncounterState.Failed || NewState == FishEncounterState.Succeeeded)
         {
-            Invoke(nameof(ResetAnimationToIdle), 0.5f);
+            Invoke(nameof(ResetAnimationToIdle), 1f);
         }
     }
 
     void ResetAnimationToIdle()
     {
+        animator.speed = 0.5f;
         animator.SetInteger("FishingState", 0);
     }
     
@@ -34,18 +36,25 @@ public class AnimationController : MonoBehaviour
         switch (NewState)
         {
             case FishEncounterState.None:
+                animator.speed = 0.2f;
                 return 0;
             case FishEncounterState.Throwing:
+                animator.speed = 1f;
                 return 1;
             case FishEncounterState.Idle:
+                animator.speed = 1f;
                 return 2;
             case FishEncounterState.Hooked:
+                animator.speed = 1f;
                 return 3; //The animator will transition to the next animation state (4) automatically
             case FishEncounterState.Caught:
+                animator.speed = 1f;
                 return 4;
             case FishEncounterState.Failed:
+                animator.speed = 1f;
                 return 5;
             case FishEncounterState.Succeeeded:
+                animator.speed = 1f;
                 return 7;
             default:
                 Debug.Log("DEFAULT in the player animation");
